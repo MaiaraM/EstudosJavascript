1395403faff0a55c0f7e112bcb4da15ac4b6a97e
MaiaraM <maiaramartins093@gmail.com>
2019-03-29 16:59:24 -0300

    typetype

---
+++ TypeType/script.js
@@ -0,0 +1,79 @@
+var frase = document.getElementById("frase");
+var contadorPalavras =  document.getElementById("palavras");
+var areaDigitacao = document.querySelector("#areaDigitacao");
+var contadortempo = document.getElementById("contadorTempo");
+var resultadoPalavras = document.getElementById("resultadoPalavras");
+var resultadoCaracteres = document.getElementById("resultadoCaracteres");
+var tempo = 20;
+var listaPalavras = frase.textContent.split(" ");
+contadorPalavras.textContent = listaPalavras.length;
+
+reset();
+
+areaDigitacao.addEventListener("focus",function(){
+    reset();
+    this.addEventListener("input", function(){
+        let caracteresDigitados = this.value.split("");
+        resultadoCaracteres.textContent = caracteresDigitados.length / tempo;
+    });
+
+
+    this.addEventListener("keypress", function(event){
+      if(event.code == 'Space'){
+        let palavrasDigitadas = this.value.split(" ");
+        verificarPalavras(palavrasDigitadas);
+      }
+    })
+    cronometro(true);
+});
+
+areaDigitacao.addEventListener("focusout", function(){
+    areaDigitacao.disabled = true;
+    cronometro(false);
+})
+
+function cronometro(status) {
+    var cronometroID;
+    if(status){
+        cronometroID = setInterval(function () {
+            contadortempo.textContent--;
+            if (contadortempo.textContent < 1) {
+                areaDigitacao.disabled = true;
+                areaDigitacao.classList.remove("bordaVermelha","bordaVerde");
+                clearInterval(cronometroID);
+            }
+        }, 1000);
+    }else{
+        cronometroID = clearInterval(cronometroID);
+    } 
+}
+
+function reset(){
+    areaDigitacao.value = "";
+    contadortempo.textContent = tempo;
+    resultadoPalavras.textContent = 0;
+    resultadoCaracteres.textContent = 0;
+}
+var aux = 0;
+
+function verificarPalavras(palavra){
+    var palavrasCertas;
+    for(var a =0 ; a <= aux ; a ++){
+        if(palavra[a].toLowerCase() == listaPalavras[a].toLowerCase()){
+            if(areaDigitacao.classList.contains("bordaVermelha")){
+                areaDigitacao.classList.replace("bordaVermelha","bordaVerde");
+            }else{
+                areaDigitacao.classList.add("bordaVerde");
+            }
+            palavrasCertas = palavra[a];
+            console.log(palavrasCertas);
+        }else{
+            if(areaDigitacao.classList.contains("bordaVerde")){
+                areaDigitacao.classList.replace("bordaVerde","bordaVermelha");
+            }else{
+                areaDigitacao.classList.add("bordaVermelha");
+            } 
+        }
+    }
+    aux++;
+}
