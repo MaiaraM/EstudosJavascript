f348b161a34a2d840bf646c8718b92ae76259ea5
Maiara <maiaramartins093@gmail.com>
2019-03-29 21:21:27 -0300

    conta resultado

--- TypeType/script.js
+++ TypeType/script.js
@@ -1,5 +1,5 @@
 var frase = document.getElementById("frase");
-var contadorPalavras =  document.getElementById("palavras");
+var contadorPalavras = document.getElementById("palavras");
 var areaDigitacao = document.querySelector("#areaDigitacao");
 var contadortempo = document.getElementById("contadorTempo");
 var resultadoPalavras = document.getElementById("resultadoPalavras");
@@ -10,45 +10,46 @@ contadorPalavras.textContent = listaPalavras.length;
 
 reset();
 
-areaDigitacao.addEventListener("focus",function(){
+areaDigitacao.addEventListener("focus", function () {
     reset();
-    this.addEventListener("input", function(){
+    this.addEventListener("input", function () {
         let caracteresDigitados = this.value.split("");
         resultadoCaracteres.textContent = caracteresDigitados.length / tempo;
     });
 
-
-    this.addEventListener("keypress", function(event){
-      if(event.code == 'Space'){
-        let palavrasDigitadas = this.value.split(" ");
-        verificarPalavras(palavrasDigitadas);
-      }
-    })
+    // this.addEventListener("keypress", function(event){
+    //   if(event.code == 'Space'){
+    //     let palavrasDigitadas = this.value.split(" ");
+    //     verificarPalavras(palavrasDigitadas);
+    //   }
+    // })
     cronometro(true);
 });
 
-areaDigitacao.addEventListener("focusout", function(){
+areaDigitacao.addEventListener("focusout", function () {
     areaDigitacao.disabled = true;
     cronometro(false);
+    contadortempo.textContent = 1;
 })
 
 function cronometro(status) {
     var cronometroID;
-    if(status){
+    if (status) {
         cronometroID = setInterval(function () {
             contadortempo.textContent--;
             if (contadortempo.textContent < 1) {
                 areaDigitacao.disabled = true;
-                areaDigitacao.classList.remove("bordaVermelha","bordaVerde");
+                resultado();
+                areaDigitacao.classList.remove("bordaVermelha", "bordaVerde");
                 clearInterval(cronometroID);
             }
         }, 1000);
-    }else{
+    } else {
         cronometroID = clearInterval(cronometroID);
-    } 
+    }
 }
 
-function reset(){
+function reset() {
     areaDigitacao.value = "";
     contadortempo.textContent = tempo;
     resultadoPalavras.textContent = 0;
@@ -56,24 +57,34 @@ function reset(){
 }
 var aux = 0;
 
-function verificarPalavras(palavra){
-    var palavrasCertas;
-    for(var a =0 ; a <= aux ; a ++){
-        if(palavra[a].toLowerCase() == listaPalavras[a].toLowerCase()){
-            if(areaDigitacao.classList.contains("bordaVermelha")){
-                areaDigitacao.classList.replace("bordaVermelha","bordaVerde");
-            }else{
+function verificarPalavras(palavra) {
+    for (var a = 0; a <= aux; a++) {
+        if (palavra[a].toLowerCase() == listaPalavras[a].toLowerCase()) {
+            if (areaDigitacao.classList.contains("bordaVermelha")) {
+                areaDigitacao.classList.replace("bordaVermelha", "bordaVerde");
+            } else {
                 areaDigitacao.classList.add("bordaVerde");
             }
-            palavrasCertas = palavra[a];
-            console.log(palavrasCertas);
-        }else{
-            if(areaDigitacao.classList.contains("bordaVerde")){
-                areaDigitacao.classList.replace("bordaVerde","bordaVermelha");
-            }else{
+        } else {
+            if (areaDigitacao.classList.contains("bordaVerde")) {
+                areaDigitacao.classList.replace("bordaVerde", "bordaVermelha");
+            } else {
                 areaDigitacao.classList.add("bordaVermelha");
-            } 
+            }
         }
     }
     aux++;
 }
+
+function resultado() {
+    var i =0  ;
+    let valores = areaDigitacao.value.split(" ");
+    for (let b = 0; b < valores.length; b++) {
+       if(valores[b] == listaPalavras[b]){
+           i++;
+       } 
+    }
+    console.log(i);
+    resultadoPalavras.textContent = i;
+}
+
